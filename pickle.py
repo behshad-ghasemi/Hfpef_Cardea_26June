ddd95e01a4ac857eb55e2789b3dd85a0ff25aed4
behshad-ghasemi <145139266+behshad-ghasemi@users.noreply.github.com>
2025-06-20 11:34:03 +0200

    Update pickle.py

--- pickle.py
+++ pickle.py
@@ -12,17 +12,8 @@ xgboost_model = joblib.load('xgboost_model.joblib')
 
 # Define feature list
 FEATURES = [ 
-    "BSA (non toccare)", 'Epicardial fat thickness (mm)', 'LVEDD (mm)', 'LVESD (mm)', 'LV mass (g)', 'LV mass i (g/m2) Calcolo automatico',
-    'LAD (mm)', 'LAV (ml)', 'iLAV (mL/m2) Calcolo automatico', 'LVEF (%)', 'MR 0=no, 1=min, 2=lieve, 3=mod, 4=sev', 'E/A', 'E/e avg',
-    'VCI diameter (mm)', 'sPAP (mmHg) = RV-RA + RAP (3-8-15)', 'Probabilità IP a riposo (1, bassa; 2, intermedia; 3, alta)', 'eta',
-    'BSA', 'IAS (0, assente; 1, presente)', 'Dislipidemia (0, assente; 1, presente, 2 s. metabolica)', 'Diabete mellito (0, assente; 1, presente, 2 diabete complicato)',
-    'Beta bloccante 1 si, 0 no', 'MRA 1 si, 0 no', 'ACE/ARB 1 si, 0 no', 'Terapia antiscompenso assente (0), sub-ottimale (1-2), ottimale (3)',
-    "Insufficienza renale lieve (si 1; no 0)", "Insufficienza renale severa (si 1; no 0)", 'classe funzionale WHO-FC',
-    'segni clinici di insufficienza cardiaca sinistra, astenia, dispnea (0, assenti; 1, presenti)',
-    'Segni clinici di insufficienz acardiaca destra, edemi (0, assenti; 1, presenti)',
-    'Qualsiasi segno di insufficienza cardiaca', 'PAS (mmHg)', 'PAD (mmHg)', 'NT-pro-BNP (pg/mL)',
-    'Fibrillazione atriale parossistica (0, assente; 1, presente)', 'Fibrillazione atriale cronica (0, assente; 1, presente)',
-    'Fumo (0 no; 1 si)', 'sesso'
+    'Epicardial fat thickness (mm)', 'LV mass i (g/m2) Calcolo automatico',
+    'LAD (mm)','LVEF (%)','E/e avg', 'DM','WHO-FC','PAS (mmHg)', 'NT-pro-BNP (pg/mL)', 'AF'
 ]
 
 @app.route('/')
